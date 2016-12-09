b9ca25b83aee79b525106684a0c7db38951692c6
Zhengyang Fang <fzy@live.unc.edu>
2016-12-08 22:08:46 -0500

    Userinfo!!!

---
+++ Back-End/orm/UserInfo.php
@@ -0,0 +1,104 @@
+<?php
+date_default_timezone_set('America/New_York');
+
+class UserInfo
+{
+	private $gender;
+	private $user_id;
+	private $dob;
+	private $portrait;
+	private $phone;
+
+	public static function connect() {
+		return new mysqli("classroom.cs.unc.edu",
+											"weijian",
+											"123456",
+											"weijiandb"
+											);
+	}
+
+	public static function getIDs() {
+		$mysqli = UserInfo::connect();
+
+		$res = $mysqli->query("select user_id from UserInfo");
+		
+
+		$id_array = array();
+
+		if ($res) {
+			while ($next_row = $res->fetch_array()) {
+				$id_array[] = intval($next_row['user_id']);
+			}
+		}
+		
+		return $id_array;
+	}
+
+	// public static function getUserInfoByphone($phone) {
+	// 	$mysqli = UserInfo::connect();
+
+ //    	$result = $mysqli->query('select * from UserInfo where phone like "%'.$phone.'%"');
+ //    	if ($result) {
+ //    	if ($result->num_rows == 0) {
+ //    		return null;
+ //    	}
+
+	// 	$UserInfo_info = $result->fetch_array();
+    	
+ //    	$gender = $UserInfo_info['gender'];
+ //    	$user_id = $UserInfo_info['user_id'];
+ //    	$dob = $UserInfo_info['dob'];
+ //    	$portrait = $UserInfo_info['portrait'];
+ //    	$phone = $UserInfo_info['phone'];
+ //    	haha = $UserInfo_info['profile_path'];
+ //    	haha = $UserInfo_info['Website'];
+ //    	haha= $UserInfo_info['preview'];
+
+ //    	return new UserInfo($gender,$user_id,$dob,$portrait,$phone,haha,haha,haha);
+ //    	}
+	// }
+
+	public static function getUserInfoByID($id) {
+		$mysqli = UserInfo::connect();
+
+    	$result = $mysqli->query("select * from UserInfo where user_id = " . $id);
+    	if ($result) {
+    		if ($result->num_rows == 0) {
+    			return null;
+    		}
+
+    	$UserInfo_info = $result->fetch_array();
+    	
+    	$gender = $UserInfo_info['gender'];
+    	$user_id = $UserInfo_info['user_id'];
+    	$dob = $UserInfo_info['dob'];
+    	$portrait = $UserInfo_info['portrait'];
+    	$phone = $UserInfo_info['phone'];
+ 
+    	return new UserInfo($gender,$user_id,$dob,$portrait,$phone);
+    	}
+	}
+	
+	public function getJSON() {
+		$json = array(
+				"gender"=>$this->gender,
+				"user_id"=>$this->user_id,
+				"dob"=>$this->dob,
+				"portrait"=>$this->portrait, 
+				"phone"=>$this->phone
+			);
+		 return json_encode($json);
+	}
+
+	private function __construct
+	($gender,$user_id,$dob,$portrait,$phone) 
+	{
+		$this->gender = $gender;
+		$this->user_id = $user_id;
+		$this->dob = $dob;
+		$this->portrait = $portrait;
+		$this->phone = $phone;
+	}
+
+}
+?>
---
+++ Back-End/userInfo.php
@@ -0,0 +1,72 @@
+<?php
+ini_set('display_errors',1);
+date_default_timezone_set('America/New_York');
+
+require_once('orm/UserInfo.php');
+
+if (isset($_SERVER['PATH_INFO'])) {
+	$path_components = explode('/', $_SERVER['PATH_INFO']);
+}else{
+	$path_components=null;
+}
+
+
+//$resource_type = $path_components[1]; // e.g. "UserInfo"
+
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+	if (count($path_components) == 0) {
+		$id = $_GET['userID'];
+		$UserInfo = UserInfo::getUserInfoByID($id);
+		if ($UserInfo == null) {
+				// UserInfo not found
+				header("Content-type: application/json");
+				print(json_encode(array("found"=>false)));
+				exit();
+			}
+			header("Content-type: application/json");
+			print($UserInfo->getJSON());
+			exit();
+	} else {
+		$resource_type = $path_components[1]; // e.g. "UserInfo"
+		if ($resource_type == "UserInfo") {
+
+			if ((count($path_components) == 2)) {      // get all UserInfo ids
+				header("Content-type: application/json");
+				print(json_encode(UserInfo::getIDs()));
+				exit();
+			} else {    // get a UserInfo's json
+				$UserInfo_id = intval($path_components[2]);
+				$UserInfo = UserInfo::getUserInfoByID($UserInfo_id);
+				if ($UserInfo == null) {
+					// UserInfo not found
+					header("HTTP/1.0 404 Not Found");
+					exit();
+				}
+				header("Content-type: application/json");
+				print($UserInfo->getJSON());
+				exit();
+			}
+		}
+	}
+	
+} else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+	/* Post tickets 
+	 
+	if ($resource_type == "games") {
+		if (count($path_components) == 3) {
+			$game_id = intval($path_components[2]);
+			$game = Game::getGameByID($game_id);
+			if ($game == null) {
+				// game not found
+				header("HTTP/1.0 404 Not Found");
+				exit();
+			}
+
+		}
+	}
+	*/
+}
+
+
+
+?>
