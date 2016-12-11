cf63e089c32c93c32ff62eaa72a66bdd6f2e143f
WeijianL1 <liweijian1996@gmail.com>
2016-12-10 20:46:49 -0500

    post function done

--- Back-End/product.php
+++ Back-End/product.php
@@ -49,21 +49,21 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 	}
 	
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
-	/* Post tickets 
-	 
-	if ($resource_type == "games") {
-		if (count($path_components) == 3) {
-			$game_id = intval($path_components[2]);
-			$game = Game::getGameByID($game_id);
-			if ($game == null) {
-				// game not found
-				header("HTTP/1.0 404 Not Found");
-				exit();
-			}
+	$resource_type=$path_components[1];
+	if ($resource_type=='product') {
+		$img=$_POST['img'];
+		$product_name=$_POST['product_name'];
+		$category=$_POST['category'];
+		$seller_id=$_POST['seller_id'];
+		$description=$_POST['description'];
+		$price=$_POST['price'];
+		
+		$create=Product::create($seller_id,$product_name,$price,$img,$description,$category);
 
-		}
+		header("Content-type: application/json");
+		print(json_encode($create));
+
 	}
-	*/
 }
 
 
