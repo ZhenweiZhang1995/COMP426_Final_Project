c3235e3190804e26c6a712b34a15817eed255490
WeijianL1 <liweijian1996@gmail.com>
2016-12-11 03:20:04 -0500

    Add login cookie and some change to the home page
    login cookie works,
    logout doesn’t

--- Front-End/js/signIn.js
+++ Front-End/js/signIn.js
@@ -1,4 +1,7 @@
  $(document).ready(function () {
+ 	if(getCookie("username")!=""){
+ 		nav_login(getCookie("username"));
+ 	}
  	 $('#sign_in').on('click', function (e) {
  	 	var pass=$("#password_login").val();
  	 	var user=$("#username_login").val();
@@ -19,6 +22,7 @@
 	        	success: function(json){ 
 	            if(json.success==1){//登陆成功
 	                alert("Log in success!");
+	                nav_login(getCookie("username"));
 	            }else if(json.success==2){//未注册用户
 	            	alert("Username not found!");
 	            }
@@ -26,23 +30,64 @@
 		       }});
 		}
     }); 
-
-    $('.forgot-pass').click(function(event) {
-      $(".pr-wrap").toggleClass("show-pass-reset");
-    }); 
-    
-    $('.pass-reset-submit').click(function(event) {
-      $(".pr-wrap").removeClass("show-pass-reset");
-    }); 
-
-
-
+ 	 //next part doesnt work
+ 	 $('#logout_button').on('click',function(e){
+ 	 	e.stopPropagation();
+		e.preventDefault();
+		document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/Courses/comp426-f16/users";
+		document.cookie = "user_id=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/Courses/comp426-f16/users";
+		alert("sdf");
+		 //$.ajax('../Back-End/login.php?action=logout',
+		  //      {type: 'GET',
+		  //      	datatype:"json",
+				// cache: false,
+	   //      	success: function(json){ 
+	   //      		alert(json);
+				// 	nav_logout();
+		  //      }
+		  //  });
+ 	 });
 });
 
-$(document).keypress(function(e) {
-    if(e.which == 13) {
-        $("#sign_in").click();
-        document.getElementById("username_login").value= "";
-        document.getElementById("password_login").value= "";
+var nav_login=function(username){
+	$('#user_nav').empty();
+	$('#user_nav').append(
+		'<li><a href="account.html">Hi, '+username+'</a></li>'+
+		'<li><a href="post.html">Post My Skill</a></li>'+
+		'<li><a href="cart.html"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Shopping Cart</a></li>'+
+		'<li id="logout_button"><a href="index.html">Sign out</a></li>'
+		);
+};
+
+var nav_logout=function(){
+	$('#user_nav').empty();
+	$('#user_nav').append(
+		'<li><a href="cart.html"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Shopping Cart</a></li>'+
+		'<li><a id="nav_register_button" href="#modal">Sign in</a></li>'
+		);
+};
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
     }
-});
+    return "";
+}
+
+
+
+// $(document).keypress(function(e) {
+//     if(e.which == 13) {
+//         $("#sign_in").click();
+//         document.getElementById("username_login").value= "";
+//         document.getElementById("password_login").value= "";
+//     }
+// });
