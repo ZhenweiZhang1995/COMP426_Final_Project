31a87cc2fbae5c3023c28a76fa24658081b6c64e
Zhengyang Fang <fzy@live.unc.edu>
2016-12-11 00:24:05 -0500

    account myposts and user info

--- Front-End/js/account.js
+++ Front-End/js/account.js
@@ -4,33 +4,79 @@
 
 var load_account_info=function(){
 
-  //  $.ajax('../Back-End/login.php/'+1,
-  //   {cache:false,
-  //   type: "GET",
-  //    dataType: "json",
-  //    success: function(json) {
-  //   var username=json.username;
-  //   var email=json.email;
-  //  }
-  // });	
+var username;
+var email;
+   $.ajax(
+    {type: "GET",
+      url: '../Back-End/login.php',
+      data:{
+        action:'login',
+    user:'zhengyang',
+    pass:'123456'
+        },
+      cache:false,
+     dataType: "json",
+     success: function(json) {
+    alert("gg");
+    username=json.username;
+    alert(username);
+    email=json.email;
+    alert(email);
+   }
+  });	
 
    $.ajax('../Back-End/userInfo.php/UserInfo/1',//currently can only get user number 1's information
     {async: true,
     type: "GET",
      dataType: "json",
      success: function(userInfo_json) {
+    $('#BigUserName').append(
+      username
+      );
     $('#UserInfo').append(
-    	'<h3>haha</h3>'
-         // '<h3>Account information</h3>'
-         //        '<p>Username: <span style="color:blue" id="UserName">'+username+'</span></p>'
-         //        '<p>Email: <span style="color:blue" id="Email">'+email+'</span></p>'
-         //        '<p>DOB: <span style="color:blue" id="DOB">'+userInfo_json.dob+'</span></p>'
-         //        '<p>Gender: <span style="color:blue" id="Gender">'+userInfo_json.gender+'</span></p>'
-         //        '<p>Phone: <span style="color:blue" id="Phone">'+userInfo_json.phone+'</span></p>'
-         //        '<p>Portrait: <span style="color:blue" id="Portrait">'+userInfo_json.portrait+'</span></p>'
+         '<h3>Account information</h3>'+
+                '<p>Username: <span style="color:blue" id="UserName">'+username+'</span></p>'+
+                '<p>Email: <span style="color:blue" id="Email">'+email+'</span></p>'+
+                '<p>DOB: <span style="color:blue" id="DOB">'+userInfo_json.dob+'</span></p>'+
+                '<p>Gender: <span style="color:blue" id="Gender">'+userInfo_json.gender+'</span></p>'+
+                '<p>Phone: <span style="color:blue" id="Phone">'+userInfo_json.phone+'</span></p>'+
+                '<p>Portrait: <span style="color:blue" id="Portrait">'+userInfo_json.portrait+'</span></p>'
                 );
+    
    }
    });
+
+   $.ajax(
+    {type: "GET",
+      url: '../Back-End/product.php/product/1',
+      cache:true,
+     dataType: "json",
+     success: function(product_json) {
+          alert(product_json.seller_id);
+      $('#posts').append(
+      '<tr>'+
+                        '<th>SellerID</th>'+
+                        '<th>ProductID</th>'+ 
+                        '<th>ProductName</th>'+
+                        '<th>Picture</th>'+
+                        '<th>Category</th>'+
+                        '<th>Description</th>'+
+                        '<th>Price</th>'+
+                    '</tr>'+
+                    '<tr>'+
+                       '<th id="SellerID">'+product_json.seller_id+'</th>'+
+                        '<th id="ProductID">'+product_json.product_id+'</th>'+
+                        '<th id="ProductName">'+product_json.product_name+'</th>'+           
+                        '<th id="Picture">'+product_json.pic_path+'</th>'+
+                        '<th id="Category">'+product_json.category+'</th>'+
+                        '<th id="Description">'+product_json.description+'</th>'+
+                        '<th id="Price">'+product_json.price+'</th>'+
+                    '</tr>'
+      );
+
+   }
+  }); 
+
 };
 
 
